8ab3322331973bd647106fbd096f79dd4ce5dee4
Lukas Heddendorp <lu.heddendorp@gmail.com>
2020-03-14 22:00:13 +0100

    Small fixes and new introduction

--- LaTeX/data/test_native_api.cpp
+++ LaTeX/data/test_native_api.cpp
@@ -1,17 +1,6 @@
-#ifndef WASM3_TEST_API_H
-#define WASM3_TEST_API_H
-
-#include <stdint.h>
-
-#define WASM_EXPORT                   extern "C" __attribute__((used)) __attribute__((visibility ("default")))
-#define WASM_EXPORT_AS(NAME)          WASM_EXPORT __attribute__((export_name(NAME)))
-#define WASM_IMPORT(MODULE, NAME)      __attribute__((import_module(MODULE))) __attribute__((import_name(NAME)))
-
 extern "C" {
 
 WASM_IMPORT("thesis", "sendValue")   uint32_t sendValue    (void);
 WASM_IMPORT("thesis", "mark")        void mark             (void);
 
 }
-
-#endif //WASM3_TEST_API_H
--- LaTeX/data/test_switch.cpp
+++ LaTeX/data/test_switch.cpp
@@ -1,5 +1,5 @@
 uint32_t run(uint32_t n) {
-    uint32_t array1[20] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    uint32_t array1[20] = {1, /*some entries omitted*/ 20};
     uint32_t result = 200;
     for (int i = 0; i < n; ++i) {
         uint32_t compare = i % 20;
